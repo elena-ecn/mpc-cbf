25ffc8f6243c41bb43f94aab9e58a231cf13289e
elena-ecn <56850846+elena-ecn@users.noreply.github.com>
2022-11-26 19:15:41 -0700

    refactor: Place comparisons in separate file

--- main.py
+++ main.py
@@ -30,11 +30,6 @@ def main():
     # Store results
     # util.save_mpc_results(controller)
 
-    # --------------------------------------------------------------------------
-    # util.compare_results_by_gamma()                   # Compares the path for each method and gamma value
-    # util.run_multiple_experiments(N=50)               # Runs N experiments for each method
-    # util.compare_controller_results(N=50, gamma=0.1)  # Compares total costs and min distances for each method
-
 
 if __name__ == '__main__':
     main()
---
+++ tests.py
@@ -0,0 +1,9 @@
+import util
+
+
+if __name__ == '__main__':
+
+    util.compare_results_by_gamma()                   # Compares the path for each method and gamma value
+
+    # util.run_multiple_experiments(N=50)               # Runs N experiments for each method
+    # util.compare_controller_results(N=50, gamma=0.1)  # Compares total costs and min distances for each method
